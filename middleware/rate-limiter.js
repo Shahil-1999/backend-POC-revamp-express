091e31cede2864cc4ee74f38f1f7bff10f6971cc
Shahil <kshahil1999@gmail.com>
2026-03-03 16:30:38 +0530

    update rate limit

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 const {rateLimit} = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs:  20000, // 20 seconds
-    max: 10, // limit each IP to 100 requests per windowMs
+    max: 10, // limit each IP to 10 requests per windowMs
     message: 'Too many requests from this IP, please try again after 20 seconds',
     statusCode: 429,
 });
